296c31cc66f1319f0ddebc085701679c68e0d5ac
Alba GC <albagoncebrian@gmail.com>
2022-05-09 15:48:12 +0100

    Update pca_opt.R
    Transformation of variables

--- R/pca_opt.R
+++ R/pca_opt.R
@@ -212,7 +212,7 @@ vpca_removecells <- function (data, A, ref.P, k_ho=1000,
       ho <- ho.list[[jmd]][[jrep]]
       Xtr <- as.matrix(X)
       Xtr[ho] <- NA
-      mod.tsr <-pcambtsrR(Xtr,A,xscale = TRUE)
+      mod.tsr <-pcambtsrR(Xtr,A, maxiter = 200)
       Xtr.imp <- mod.tsr$X
       
       try({
@@ -228,7 +228,7 @@ vpca_removecells <- function (data, A, ref.P, k_ho=1000,
         para_test$limspe[id.loc] <- pcamodel_test$model$limspe
         para_test$limt2[id.loc] <- pcamodel_test$model$limt2
         para_test$log10msie[id.loc] <- log10(mean((X[ho] - Xtr.imp[ho])^2))
-        para_test$log10mspe[id.loc] <- log10(model$loss)
+        para_test$log10mspe[id.loc] <- log10(pcamodel_test$loss)
       })
     }
     d.sum[jmd,] <- paste0(round(colMeans(para_test[para_test$RWoutpctge == rm_pctges[jmd],cols.sum], 
@@ -327,13 +327,13 @@ vpca_removecols <- function (data, A, ref.P, k_ho=1000,
 #' @export
 #' 
 vpca_transcols <- function (data, A, ref.P, k_ho=1000,
-                             rm_pctges=c(1,5,10,seq(20,80,by=20)),
+                             rm_pctges=c(1,5,10,seq(20,100,by=20)),
                              ho.part = NULL){
-  X <- dat
+  X <- as.matrix(data)
   nrep <- k_ho
   nlevels <- length(rm_pctges)
   n.results <- nrep*nlevels
-  
+  n.elems <- ncol(X)
   # Build list containing the results
   para_test <- list(limspe = matrix(NA,n.results,1), 
                     limt2 = matrix(NA,n.results,1), 
@@ -351,21 +351,22 @@ vpca_transcols <- function (data, A, ref.P, k_ho=1000,
   cols.sum <- which(!(colnames(para_test)%in%c("Repetition", "RWoutpctge")))
   d.sum <- data.frame(matrix(NA,nlevels, length(cols.sum)))
   rownames(d.sum) <- paste0("rw rem ", rm_pctges)
-  ho.list <- vector(mode = "list", length = nlevels)
+  if(is.null(ho.part)){
+    ho.list <- vector(mode = "list", length = nlevels)
+  }
   for (jmd in 1:nlevels){
-    print(paste0("Rows removal at ", rm_pctges[jmd], " %"))
+    print(paste0("Columns transformation at ", rm_pctges[jmd], " %"))
     print("Parameter avge (sd)")
-    N.lim <- floor(nrow(X) - rm_pctges[jmd]/100*nrow(X))
     if(is.null(ho.part)){
-      ho  <- createTimeSlices(c(1:nrow(X)), N.lim, horizon = 1, 
-                              fixedWindow = TRUE, skip = 0)
-      ho.list[[jmd]] <- ho$train
-    } else {
-      ho.list[[jmd]] <- ho.part[[jmd]]
+      ho.list[[jmd]] <- vector(mode = "list", length = nrep)
     }
-    for (jrep in c(1:length(ho.list[[jmd]]))){
+    for (jrep in c(1:nrep)){
+      if(is.null(ho.part)){
+        ho.list[[jmd]][[jrep]] <- sample(c(1:n.elems), round(rm_pctges[jmd]/100*n.elems))
+      }
       ho <- ho.list[[jmd]][[jrep]]
-      Xtr <- X[ho,,drop=F]
+      Xtr <- X
+      Xtr[,ho] <- log(X[,ho,drop=F])
       try({
         pcamodel_test <- fit_pca(Xtr, A)
         Xrec_cv <- sweep(sweep(pcamodel_test$model$x%*%t(pcamodel_test$model$rotation),2,
@@ -378,7 +379,7 @@ vpca_transcols <- function (data, A, ref.P, k_ho=1000,
         id.loc <- and((para_test$Repetition==jrep),(para_test$RWoutpctge==rm_pctges[jmd]))
         para_test$limspe[id.loc] <- pcamodel_test$model$limspe
         para_test$limt2[id.loc] <- pcamodel_test$model$limt2
-        para_test$log10mspe[id.loc] <- log10(model$loss)
+        para_test$log10mspe[id.loc] <- log10(pcamodel_test$loss)
       })
     }
     d.sum[jmd,] <- paste0(round(colMeans(para_test[para_test$RWoutpctge == rm_pctges[jmd],cols.sum], 
