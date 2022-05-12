c14125569d940c3a189aaa930fc015713abcd2a6
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2022-05-12 14:28:39 +0100

    Add new script to make it easy to install required packages on Virtual machines

---
+++ scripts/vm_setup.R
@@ -0,0 +1,10 @@
+pkgs <- c("rmarkdown",
+          "tidyverse",
+          "caret",
+          "keras",
+          "tensorflow",
+          "tictoc",
+          "factoextra",
+          "qqplotr")
+
+install.packages(pkgs)
