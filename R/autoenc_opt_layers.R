150f767d6e80645ecbed0a5fe1f0e176fab98c3c
Alba GC <albagoncebrian@gmail.com>
2022-05-23 15:43:27 +0100

    ae opt layers function

--- R/autoenc_opt_layers.R
+++ R/autoenc_opt_layers.R
@@ -6,8 +6,8 @@
 #'
 #' @return loss_results optimal autoencoder configuration
 #' @export
-autoenc_opt <- function(dat, A.values = c(1:10), tr = NULL, kcv = 10, 
-                        act.fun = act.fun, n.epochs = n.epochs){
+autoenc_opt_layers <- function(dat, A, tr = NULL, kcv = 10, 
+                               act.fun = act.fun, n.epochs = n.epochs){
   X <- as.matrix(dat)
   # If training and testing partitions of the cross validation are not supplied 
   # in tr, then create them:
@@ -23,24 +23,45 @@ autoenc_opt <- function(dat, A.values = c(1:10), tr = NULL, kcv = 10,
   } else {
     kcv <- length(tr)
   }
-  maxA <- max(A.values)
+  n_lay <- 2^seq(0,floor(sqrt(ncol(Xtr))),by=1)
+  n_lay <- n_lay[and(n_lay > A, n_lay < ncol(Xtr))] 
+  n_lay <- sort(c(n_lay, ncol(Xtr)),decreasing = TRUE)
   # Initialise the matrix storing the results of the loss function to be optimised
-  loss_opt_tr <- matrix(NA, maxA, kcv)
-  loss_opt_ts <- matrix(NA, maxA, kcv)
-  loss_opt_ts_cv <- matrix(NA, maxA, kcv)
+  loss_opt_tr <- matrix(NA, length(n_lay), kcv)
+  loss_opt_ts <- matrix(NA, length(n_lay), kcv)
+  loss_opt_ts_cv <- matrix(NA, length(n_lay), kcv)
   # library(keras)
   
-  for (k.A in A.values){
-    
+  for (k.A in c(1:length(n_lay))){
+    print(paste("N layers at ", n_lay[k.A]))
     modelA <- keras_model_sequential()
     modelA %>%
       layer_dense(units=ncol(Xtr), activation = act.fun, input_shape = ncol(Xtr), 
-                  use_bias = TRUE, name = "input") %>%
-      layer_dense(units= k.A, activation = act.fun, input_shape = ncol(Xtr), 
-                  use_bias = TRUE, name = "latent") %>%
-      layer_dense(units=ncol(Xtr), activation = act.fun, input_shape = k.A, 
-                  use_bias = TRUE, name = "output")
-    
+                  use_bias = TRUE, name = "input")
+    if (k.A > 1){
+      for(k.layer in c(2:k.A)){
+        modelA %>%
+          layer_dense(units=n_lay[k.layer], activation = act.fun, input_shape = n_lay[k.layer-1], 
+                      use_bias = TRUE, name = paste0("hidd_in_",n_lay[k.layer]))
+      }
+      modelA %>%
+        layer_dense(units=A, activation = act.fun, input_shape = ncol(Xtr), 
+                    use_bias = TRUE, name = "latent") 
+      for(k.layer in seq(k.A,2,by=-1)){
+        modelA %>%
+          layer_dense(units=n_lay[k.layer], activation = act.fun, input_shape = n_lay[k.layer+1], 
+                      use_bias = TRUE, name = paste0("hidd_out_",n_lay[k.layer]))
+      }
+      modelA %>%
+        layer_dense(units=ncol(Xtr), activation = act.fun, input_shape = k.A, 
+                    use_bias = TRUE, name = "output")
+    } else {
+      modelA %>%
+        layer_dense(units=A, activation = act.fun, input_shape = ncol(Xtr), 
+                    use_bias = TRUE, name = "latent") %>%
+        layer_dense(units=ncol(Xtr), activation = act.fun, input_shape = A, 
+                    use_bias = TRUE, name = "output")
+    }
     summary(modelA)
     # Compile the model
     modelA %>% compile(
@@ -68,10 +89,8 @@ autoenc_opt <- function(dat, A.values = c(1:10), tr = NULL, kcv = 10,
   return(loss_results = list(losstr = loss_opt_tr, 
                              lossts = loss_opt_ts,
                              losstscv = loss_opt_ts_cv,
-                             A = A.values, 
-                             tr = tr))
+                             A = A, 
+                             tr = tr,
+                             n_lay=n_lay))
   
 }
-
-
-
