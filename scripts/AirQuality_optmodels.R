26399ceae30fc38c6e1a87f7b188b9da5cad8c49
Alba GC <albagoncebrian@gmail.com>
2022-05-26 23:30:00 +0100

    Create AirQuality_optmodels.R

---
+++ scripts/AirQuality_optmodels.R
@@ -0,0 +1,51 @@
+library(dplyr)
+library(magrittr)
+library(factoextra)
+library(qqplotr)
+library(tictoc)
+library(caret)
+library(keras)
+library(ggplot2)
+library(tidyr)
+library(tensorflow)
+source(here::here("pcals.R"))
+source(here::here("autoencoder.R"))
+source(here::here("autoencoder_opt.R"))
+source(here::here("autoenc_opt_layers.R"))
+source(here::here("pca_functions.R"))
+source(here::here("pcambtsrR.R"))
+source(here::here("lsdAnalysis.R"))
+source(here::here("lsdfig.R"))
+
+
+## ----load data----------------------------------------------------------------------------------------------------
+dat <- readxl::read_xlsx(here::here("data","AirQualityUCI.xlsx")) %>%
+  select_if(is.numeric) %>%
+  filter(complete.cases(.))
+
+
+## ----delnullvar, out.width='70%'----------------------------------------------------------------------------------
+print("These variables show null variance. Consider their deletion before the PCA")
+summary(dat[,(apply(dat,2,var)==0),drop=F])
+dat.or <- dat
+dat <- dat[,!(apply(dat,2,var)==0),drop=F]
+
+## ----univanalysis-------------------------------------------------------------------------------------------------
+summary(dat)
+boxplot(dat,
+        data=dat,
+        main="Air Quality dataset",
+        xlab="Parameter",
+        ylab="",
+        col="green3",
+        border="black"
+)
+dat %>% gather() %>% head()
+tic("Autoencoder optimization")
+# opt.AE.results <- autoenc_opt(dat, A.values = c(1:10), tr = NULL, kcv = 20, act.fun = "relu", n.epochs = 50)
+opt.AElay.results <- autoenc_opt_layers(dat, A=6, tr = NULL, kcv = 20,act.fun = "relu", n.epochs = 50)
+time.EA.opt <- toc()
+
+tic("PCA optimization")
+opt.PCA.results <- pca_opt(dat, A.values = c(1:10), tr = opt.AE.results$tr, kcv = 20)
+toc()
