d4de465841451a7ab4f0b42b2a3cb19d8c9af9f7
Alba GC <albagoncebrian@gmail.com>
2022-05-26 22:59:33 +0100

    Experiments for Air Quality

---
+++ scripts/AirQuality_experiment_2transrows.R
@@ -0,0 +1,42 @@
+library(dplyr)
+library(magrittr)
+library(factoextra)
+library(qqplotr)
+library(tictoc)
+library(caret)
+library(keras)
+library(ggplot2)
+library(tidyr)
+library(tensorflow)
+source(here::here("pcals.R"))
+source(here::here("autoencoder.R"))
+source(here::here("autoencoder_opt.R"))
+source(here::here("autoenc_opt_layers.R"))
+source(here::here("pca_functions.R"))
+source(here::here("pcambtsrR.R"))
+source(here::here("lsdAnalysis.R"))
+source(here::here("lsdfig.R"))
+
+
+## ----load data----------------------------------------------------------------------------------------------------
+dat <- readxl::read_xlsx(here::here("data","AirQualityUCI.xlsx")) %>%
+  select_if(is.numeric) %>%
+  filter(complete.cases(.))
+
+
+## ----delnullvar, out.width='70%'----------------------------------------------------------------------------------
+print("These variables show null variance. Consider their deletion before the PCA")
+summary(dat[,(apply(dat,2,var)==0),drop=F])
+dat.or <- dat
+dat <- dat[,!(apply(dat,2,var)==0),drop=F]
+
+## ----transform, results="hide", echo=FALSE,out.width="50%",fig.asp=0.9--------------------------------------------
+pca_transvars <- vpca_rowpctge(data = dat, 6, ref.P = P.pca.ref, 
+                               k_ho = 10, rm_pctges = c(5,10,seq(20,80,by=20)))
+
+autoencoder_transvars <- vae_rowpctge(data = dat, 6, 
+                                      ref.P = P.ae.ref,
+                                      ho.part = pca_transvars$ho,
+                                      k_ho = 10, 
+                                      rm_pctges = c(5,10,seq(20,80,by=20)))
+save(list = c("pca_transvars", "autoencoder_transvars"),file="AQ_RowsPctge_models.RData")
---
+++ scripts/AirQuality_experiment_4remrows.R
@@ -0,0 +1,39 @@
+library(dplyr)
+library(magrittr)
+library(factoextra)
+library(qqplotr)
+library(tictoc)
+library(caret)
+library(keras)
+library(ggplot2)
+library(tidyr)
+library(tensorflow)
+source(here::here("pcals.R"))
+source(here::here("autoencoder.R"))
+source(here::here("autoencoder_opt.R"))
+source(here::here("autoenc_opt_layers.R"))
+source(here::here("pca_functions.R"))
+source(here::here("pcambtsrR.R"))
+source(here::here("lsdAnalysis.R"))
+source(here::here("lsdfig.R"))
+
+
+## ----load data----------------------------------------------------------------------------------------------------
+dat <- readxl::read_xlsx(here::here("data","AirQualityUCI.xlsx")) %>%
+  select_if(is.numeric) %>%
+  filter(complete.cases(.))
+
+
+## ----delnullvar, out.width='70%'----------------------------------------------------------------------------------
+print("These variables show null variance. Consider their deletion before the PCA")
+summary(dat[,(apply(dat,2,var)==0),drop=F])
+dat.or <- dat
+dat <- dat[,!(apply(dat,2,var)==0),drop=F]
+
+## ----remrows, results="hide",out.width="50%",fig.asp=0.9----------------------------------------------------------
+pca_remrows <- vpca_removerows(data = dat, 6, ref.P = P.pca.ref, k_ho = 20, 
+                               rm_pctges = c(2,5,10,seq(20,80,by=20)))
+autoencoder_remrows <- vautoencoder_removerows(data = dat, 6, ref.P = P.ae.ref,k_ho = 20, 
+                                               ho.part = pca_remrows$ho,
+                                               rm_pctges = c(2,5,10,seq(20,80,by=20)))
+save(list = c("pca_remrows", "autoencoder_remrows"),file="AQ_RemRows_models.RData")
